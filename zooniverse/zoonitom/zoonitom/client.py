eacaecfe0a7664eb6eed7d5f2992a65b638d63ff
Adam McMaster <me@adam-mcmaster.com>
2025-06-24 16:00:42 +0100

    Start setting up Zooniverse client

---
+++ zooniverse/zoonitom/zoonitom/client.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+
+from panoptes_client import Panoptes, Project, Workflow
+
+if (
+    settings.ZOONIVERSE_CLIENT_ID
+    and settings.ZOONIVERSE_CLIENT_SECRET
+    and not Panoptes.client().logged_in()
+):
+    Panoptes.connect(
+        client_id=settings.ZOONIVERSE_CLIENT_ID,
+        client_secret=settings.ZOONIVERSE_CLIENT_SECRET,
+    )
+
+
+project = Project(settings.ZOONIVERSE_PROJECT_ID)
+workflow = Workflow(settings.ZOONIVERSE_WORKFLOW_ID)
