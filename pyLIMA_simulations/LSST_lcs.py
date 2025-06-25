96b828b7669a2a8b340903acf8919fed6a29907e
ebachelet <etibachelet@gmail.com>
2025-06-24 17:24:21 -0700

    Add pyLIMA simulations

---
+++ pyLIMA_simulations/LSST_lcs.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import simulate_lc as smlc
+
+time = np.arange(2461000,2462000,50)
+ra = 270
+dec = -28
+
+#adjust tetelescope ZP, u,g,r,i,z,y
+zps = [27.8,28,28.2,28.4,28,27.2]
+
+u = smlc.simulate_one_LSST_band(time,band_name='u')
+g = smlc.simulate_one_LSST_band(time,band_name='g')
+r = smlc.simulate_one_LSST_band(time,band_name='r')
+i = smlc.simulate_one_LSST_band(time,band_name='i')
+z = smlc.simulate_one_LSST_band(time,band_name='z')
+y = smlc.simulate_one_LSST_band(time,band_name='y')
+
+ml_event =   smlc.simulate_a_microlensing_event(ra,dec,[u,g,r,i,z,y])
+#lcs in fluxes!
+lcs,ml_model,pyLIMA_parameters = smlc.simulate_lcs(ml_event,model_choice='USBL',zps=zps)
+
+for ind,lc in enumerate(lcs):
+    plt.scatter(time,zps[ind]-2.5*np.log10(lc))
+
+plt.gca().invert_yaxis()
+plt.show()
+
+breakpoint()
---
+++ pyLIMA_simulations/simulate_lc.py
@@ -0,0 +1,102 @@
+import numpy as np
+import pyLIMA.event
+import pyLIMA.models.PSPL_model
+import pyLIMA.models.USBL_model
+import pyLIMA.simulations.simulator
+
+
+
+def simulate_one_LSST_band(time,band_name='g'):
+
+    telo = pyLIMA.simulations.simulator.simulate_a_telescope("Rubin_"+band_name, timestamps=time, location="Earth", astrometry=False,)
+    
+    return telo
+    
+def simulate_a_microlensing_event(ra,dec,telescopes_list):
+
+    ml_event = pyLIMA.event.Event(ra=ra, dec=dec)
+    ml_event.name = "Rubin_" + str(ra) + "_" + str(dec)
+    
+    for telo in telescopes_list:
+        ml_event.telescopes.append(telo)
+
+    return ml_event
+    
+def refine_microlensing_parameters(themodel, microlensing_parameters, time):
+   
+
+    # Ensure t0 is within the observations
+    t0min, t0max = np.percentile(time, [20, 80])
+
+    new_ml_parameters = np.copy(microlensing_parameters)
+    new_ml_parameters[0] = np.random.uniform(t0min, t0max)       
+
+    new_tE = 10 ** (np.random.normal(1.2, 0.3))
+    new_ml_parameters[2] = new_tE
+
+    if themodel.model_type()=='USBL':
+
+        # Rho 
+        new_ml_parameters[3] = 10 ** np.random.uniform(-4.5, -1.30)
+
+        # Impact parameter u0 -- the following ensures some caustic signal
+        new_ml_parameters[1] = np.random.uniform(-1 * new_ml_parameters[3], 1 * new_ml_parameters[3])
+
+        # Separation 
+        new_ml_parameters[4] = 10 ** np.random.uniform(-0.5, 0.5)
+
+        # Mass ratio
+        new_ml_parameters[5] = np.random.uniform(10**-5,1)
+
+
+    return new_ml_parameters
+
+def refine_flux_parameters(zp=27.85):
+
+    f_source = 10 ** ((zp - baseline) / 2.5)
+    g_blend = np.random.uniform(0, 1)
+
+    fblend = f_source * g_blend
+
+    return f_source,f_blend
+    
+def simulate_lcs(ml_event,model_choice='PSPL',zps=[]):
+
+    if model_choice == 'PSPL':
+
+        themodel = pyLIMA.models.PSPL_model.PSPLmodel(ml_event,blend_flux_parameter='fblend',)
+
+    if model_choice == 'USBL':
+
+        choice = np.random.choice(["central_caustic", "second_caustic", "third_caustic"])
+        themodel = pyLIMA.models.USBL_model.USBLmodel(ml_event, origin=[choice, [0, 0]],blend_flux_parameter='fblend')
+    
+    the_parameters = (pyLIMA.simulations.simulator.simulate_microlensing_model_parameters(themodel))
+    all_time = []
+    for tel in ml_event.telescopes:
+       all_time.append(tel.lightcurve['time'].value)
+       
+       
+    all_time = np.unique(all_time)    
+    the_parameters = refine_microlensing_parameters(themodel, the_parameters, all_time)
+    pyLIMA_parameters = themodel.compute_pyLIMA_parameters(the_parameters)
+
+    lcs = []
+    
+    for ind,telo in enumerate(ml_event.telescopes):
+    
+        try:
+            new_fs, new_fb = refine_flux_parameters(zp=zps[ind])
+            pyLIMA_parameters['fsource_'+str(telo.name)] = new_fs
+            pyLIMA_parameters['fblend_'+str(telo.name)] = new_fb
+        except:
+            pass
+
+        magnification = themodel.model_magnification(telo, pyLIMA_parameters)
+        flux = pyLIMA_parameters['fsource_'+str(telo.name)] * magnification +  pyLIMA_parameters['fblend_'+str(telo.name)]
+        
+        lcs.append(flux)
+        
+        
+    return lcs,themodel,pyLIMA_parameters
+
